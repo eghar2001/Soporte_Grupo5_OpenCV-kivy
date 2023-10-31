0e71597286751131809eeea17eda8e1e65bdb582
eghar2001 <nahucarc2@gmail.com>
2023-10-31 17:32:06 -0300

    Se agrego funcionalidad para grabar video y tomar foto
    Falta ver las fotos y videos desde la app

--- custom_components.py
+++ custom_components.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from datetime import datetime
+
 import cv2
 from google.protobuf.json_format import MessageToDict
 from kivy.clock import Clock
@@ -6,7 +9,7 @@ from kivy.uix.camera import Camera
 import mediapipe as mp
 
 
-
+ruta_videos = "videos_grabados\\"
 class CameraOpenCv(Camera):
     """Componente que carga la camara usando opencv"""
     def __init__(self, hasFacialRecognition = False, hasHandsRecognition = False, **kwargs):
@@ -14,10 +17,16 @@ class CameraOpenCv(Camera):
         self.frame = None
         self.hasFacialRecognition = hasFacialRecognition
         self.hasHandsRecognition = hasHandsRecognition
+        self.countFaces = 0
+        self.countRightHands = 0
+        self.countLeftHands = 0
+        self.recording = False
+
         self._mp_hands_detection = mp.solutions.hands
         self._mp_face_detection = mp.solutions.face_detection
-        self._capture = cv2.VideoCapture(0)
+        self._capture = None
         self._schedule = None
+        self._video_writer = None
 
     def on_enter(self):
         print("Video abierto")
@@ -28,13 +37,42 @@ class CameraOpenCv(Camera):
         self.stop_video()
 
     def start_video(self, *args):
-        self._schedule = Clock.schedule_interval(self.load_video, 1 / 60)
+        self._capture = cv2.VideoCapture(0)
+        self._schedule = Clock.schedule_interval(self.load_video, 1 / 24)
 
     def stop_video(self, *args):
+        if self._schedule:
+            self._schedule.cancel()
+            self._capture.release()
+    def start_recording(self, *args):
+        self.recording = True
+        fecha_hora_actual = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        width = int(self._capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(self._capture.get(cv2.CAP_PROP_FPS))
+        self._video_writer = cv2.VideoWriter(f"{ruta_videos}{fecha_hora_actual}.avi",cv2.VideoWriter_fourcc(*'XVID'),fps, (width, height))
 
-        if not self._schedule:
-            return
-        self._schedule.cancel()
+    def stop_recording(self,*args):
+        if self._video_writer:
+            self._video_writer.release()
+        self.recording = False
+
+    def hasRightHands(self):
+        return self.countRightHands>=1
+
+    def hasLeftHands(self):
+        return self.countLeftHands>=1
+
+    def hasFaces(self):
+        return self.countFaces >= 1
+
+    def countHands(self):
+        return self.countLeftHands + self.countRightHands
+
+    def hasHands(self):
+        return self.countHands() >=1
+
+
 
 
     def load_video(self, *args):
@@ -42,13 +80,31 @@ class CameraOpenCv(Camera):
         if not ret:
             return
         frame = cv2.flip(frame, 1)
+        self.frame = deepcopy(frame)
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if self.recording:
+            self._video_writer.write(frame)
+        if self.hasFacialRecognition:
+            with self._mp_face_detection.FaceDetection() as face_detection:
+                results = face_detection.process(frame_rgb)
+                height, width, _ = frame.shape
+                if results.detections:
+                    self.countFaces = len(results.detections)
+                    for detection in results.detections:
+                        xmin = int(detection.location_data.relative_bounding_box.xmin * width)
+                        ymin = int(detection.location_data.relative_bounding_box.ymin * height)
+                        xmax = xmin + int(detection.location_data.relative_bounding_box.width * width)
+                        ymax = ymin + int(detection.location_data.relative_bounding_box.height * height)
+                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 1)
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        cv2.putText(frame, 'Cara', (xmin, ymax), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
         if self.hasHandsRecognition:
             with self._mp_hands_detection.Hands() as hands_detection:
                 results = hands_detection.process(frame_rgb)
                 height, width, _ = frame.shape
                 if results.multi_hand_landmarks is not None:
                     # Dibujando los puntos y las conexiones mediante mp_drawing
+                    tipos_mano = []
                     for (hand_landmarks, handedness) in zip(results.multi_hand_landmarks, results.multi_handedness):
                         y_1 = int(hand_landmarks.landmark[self._mp_hands_detection.HandLandmark.WRIST].y * height)
                         y_2 = int(hand_landmarks.landmark[self._mp_hands_detection.HandLandmark.MIDDLE_FINGER_TIP].y * height)
@@ -60,28 +116,23 @@ class CameraOpenCv(Camera):
                         y_text = max([y_1, y_2]) + 5
                         font = cv2.FONT_HERSHEY_SIMPLEX
                         tipo_mano = MessageToDict(handedness)["classification"][0]["label"]
-
+                        tipos_mano.append(tipo_mano)
                         if tipo_mano == "Right":
                             cv2.putText(frame, 'Derecha', (x_text, y_text), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+
+
                         else:
                             cv2.putText(frame, 'Izquierda', (x_text, y_text), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-        if self.hasFacialRecognition:
-            with self._mp_face_detection.FaceDetection() as face_detection:
-                results = face_detection.process(frame_rgb)
-                height, width, _ = frame.shape
-                if results.detections:
-                    for detection in results.detections:
-                        xmin = int(detection.location_data.relative_bounding_box.xmin * width)
-                        ymin = int(detection.location_data.relative_bounding_box.ymin * height)
-                        xmax = xmin + int(detection.location_data.relative_bounding_box.width * width)
-                        ymax = ymin + int(detection.location_data.relative_bounding_box.height * height)
-                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 1)
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        cv2.putText(frame, 'Cara', (xmin, ymax), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                    countRightHands = tipos_mano.count("Right")
+                    countLeftHands = tipos_mano.count("Left")
+                    self.countRightHands = countRightHands
+                    self.countLeftHands = countLeftHands
+                else:
+                    self.countRightHands = 0
+                    self.countLeftHands = 0
         frame = cv2.flip(frame, 0)
         buffer = frame.tobytes()
         texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt="bgr")
         texture.blit_buffer(buffer, colorfmt="bgr", bufferfmt="ubyte")
         self.texture = texture
-        frame = cv2.flip(frame, 0)
-        self.frame = frame
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import os
 from copy import deepcopy
 from functools import partial
@@ -6,12 +6,13 @@ import mediapipe as mp
 import cv2
 from deepface import DeepFace
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
 from custom_components import CameraOpenCv
 ruta_imagenes_login = "fotos_login\\"
-
+ruta_imagenes = "fotos_tomadas\\"
 
 
 
@@ -78,7 +79,7 @@ class RegisterScreen(Screen):
             if not results.detections:
                 print("No hay ninguna cara")
                 return False
-        fecha_hora_actual = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        fecha_hora_actual = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         cv2.imwrite(f"{ruta_imagenes_login}{fecha_hora_actual}.jpg",frame)
         print("Usuario creado con exito!!")
 
@@ -94,11 +95,79 @@ class MenuScreen(Screen):
         self.camera_menu.hasFacialRecognition = True
         self.camera_menu.hasHandsRecognition = True
 
+
+        self._seconds_left_hand = 0
+        self._mano_derecha_levantada_anterior = False
+        self._update_time = 1/2
+        self._imagen_valida = True
+        self._scheduleValidar = None
+        self._scheduleLeftHand = None
+        self._scheduleRightHand = None
+
+
     def on_enter(self):
         self.camera_menu.start_video()
+        self._scheduleUpdate = Clock.schedule_interval(self._update, self._update_time)
 
     def on_leave(self):
         self.camera_menu.stop_video()
+        if self._scheduleUpdate:
+            self._scheduleUpdate.cancel()
+
+    def _reset(self):
+        self._seconds_left_hand = 0
+        self._mano_derecha_levantada_anterior = False
+
+    def _update(self, *args):
+        if self.camera_menu.hasRightHands() and self.camera_menu.hasLeftHands():
+            self._reset()
+            print("No se pueden tener ambas manos a la vez")
+            return
+        if not self.camera_menu.hasFaces():
+            self._reset()
+            print("Debe haber al menos una cara")
+            return
+        mano_derecha_levantada = self.camera_menu.hasRightHands()
+        mano_izquierda_levantada  = self.camera_menu.hasLeftHands()
+        if mano_izquierda_levantada:
+            if self._seconds_left_hand % 1 == 0:
+                print(f"Faltan {5 - self._seconds_left_hand} segundos para tomar una foto")
+            if 0 <= self._seconds_left_hand < 5 :
+                self._seconds_left_hand += self._update_time
+            elif (self._seconds_left_hand >= 5):
+                fecha_hora_actual = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+                cv2.imwrite(f"{ruta_imagenes}{fecha_hora_actual}.jpg", self.camera_menu.frame)
+                print("Se ha tomado una foto!")
+                self._seconds_left_hand = 0
+        else:
+            self._seconds_left_hand = 0
+
+
+        if not self._mano_derecha_levantada_anterior and mano_derecha_levantada:
+            print("Se empezó a grabar")
+            self.camera_menu.start_recording()
+        if self._mano_derecha_levantada_anterior and not mano_derecha_levantada:
+            print("Se dejo de grabar")
+            self.camera_menu.stop_recording()
+        self._mano_derecha_levantada_anterior = mano_derecha_levantada
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
